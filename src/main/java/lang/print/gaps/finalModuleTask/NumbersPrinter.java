32294c13039faf8a1f3361db3bc86566cf9456c3
2Aram1Hakobyan2 <ar2hakobyan1@yahoo.com>
2023-07-04 22:30:18 +0400

    done, done, done

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.finalModuleTask;
-
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        boolean result = number>0;
+        System.out.println(result);
     }
 }
